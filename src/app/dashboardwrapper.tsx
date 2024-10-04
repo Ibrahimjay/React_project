376458da919dcb8a088c1a58e7ef8b5a48036db8
Ibrahimjay <jallohim130@gmail.com>
2024-10-04 16:24:27 +0000

    minor aditions

--- src/app/dashboardwrapper.tsx
+++ src/app/dashboardwrapper.tsx
@@ -9,7 +9,6 @@ import state from '@/state';
 const DashboardLayout = ({ children} : {children: React.ReactNode}) => {
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed
-
   );
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
@@ -22,7 +21,7 @@ const DashboardLayout = ({ children} : {children: React.ReactNode}) => {
   })
 
   return (
-    <div className={` flex bg-gray-50 text-gray-900 w-full min-h-screen `}>
+    <div className={` ${ isDarkMode ? "dark" : "light" } flex bg-gray-50 text-gray-900 w-full min-h-screen `}>
       <Sidebar/>
       <main className={` flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${ isSidebarCollapsed ? "md:pl-24" : "md:pl-72"} `} >
         
